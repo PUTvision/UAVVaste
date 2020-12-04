7603b07a829270b605057fe6f2df0ac33db8d3be
Bartosz <bartptak@gmail.com>
2020-12-04 11:48:22 +0100

    Add download script

---
+++ main.py
@@ -0,0 +1,32 @@
+import csv
+import requests
+from pycocotools.coco import COCO
+from tqdm import tqdm
+
+anns_path = './annotations/annotations.json'
+dest_path = './images/'
+
+
+
+def try_download(source: str, dest: str, name: str):
+    try:
+        r = requests.get(source, allow_redirects=True)
+        with open(f'{dest}{name}', 'wb') as f:
+            f.write(r.content)
+        
+    except:
+        print(f'Failed to download the file: {name}')
+
+def download(img: dict, dest_path: str):
+    if img['flickr_url'] is not None:
+        try_download(img['flickr_url'], dest_path, img['file_name'])
+
+def main():
+    dataset = COCO(anns_path)
+
+    for img in tqdm(dataset.imgs.values()):
+        download(img, dest_path)
+
+
+if __name__ == '__main__':
+    main()
